19cec3e9c3e8b2a0e3938ba64a97321bfd923a5f
Zekaroni <ezekiel.hall.04@gmail.com>
2026-05-10 18:42:35 -0400

    push for final

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lib.SC_BNO055 import BNO055
 from lib.SC_CAMERA import RocketCamera
 from lib.SC_DS18B20 import DS18B20
 from lib.SC_LOGGING import DataLog, BNOLogger
-from lib.SC_MLX90393 import MLX90393
+# from lib.SC_MLX90393 import MLX90393
 from lib.SC_PIZERO2W import PiZeroBoard
 
 from config import SystemsPollingRates
@@ -28,12 +28,12 @@ def bno_loop(runID, i2c):
             logger.logRawBytes(ts, current_buffer)
             last_buffer[:] = current_buffer
 
-def mlx_loop(runID, i2c):
-    mlx90393 = MLX90393(i2c)
-    logger = DataLog(runID, "mlx90393") 
-    while True:
-        logger.write(f"{time.time()},{mlx90393.magnetic}")
-        time.sleep(1/SystemsPollingRates.MLX90393) 
+# def mlx_loop(runID, i2c):
+#     mlx90393 = MLX90393(i2c)
+#     logger = DataLog(runID, "mlx90393") 
+#     while True:
+#         logger.write(f"{time.time()},{mlx90393.magnetic}")
+#         time.sleep(1/SystemsPollingRates.MLX90393) 
 
 def temp_loop(runID):
     ds18b20 = DS18B20()
@@ -60,13 +60,12 @@ if __name__ == "__main__":
     cam.startRecording()
     
     threading.Thread(target=bno_loop, args=(runID, i2c), daemon=True).start()
-    threading.Thread(target=mlx_loop, args=(runID, i2c), daemon=True).start()
+    # threading.Thread(target=mlx_loop, args=(runID, i2c), daemon=True).start()
     threading.Thread(target=temp_loop, args=(runID,), daemon=True).start()
     threading.Thread(target=sys_loop, args=(runID,), daemon=True).start()
     
     try:
         while True:
-            time.sleep(1)
-            
-    except KeyboardInterrupt:
-        cam.stopRecording()
+            time.sleep(1)        
+    except Exception:
+        pass
